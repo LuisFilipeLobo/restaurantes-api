d5f035cdfa829b86fddb17630a51d895aa72437e
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2022-11-27 04:50:34 -0300

    Adicionando base do projeto

---
+++ src/main/java/br/com/allfood/restaurantesapi/RestaurantesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.allfood.restaurantesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/allfood/restaurantesapi/RestaurantesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.allfood.restaurantesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
