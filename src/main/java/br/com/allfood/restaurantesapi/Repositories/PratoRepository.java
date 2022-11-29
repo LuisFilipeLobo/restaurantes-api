971a074d86bb907c327027ccfa946b76942cb0b8
Luis Filipe Lobo <luisfilipelobo.dev@gmail.com>
2022-11-29 08:13:43 -0300

    Adicionando repositorio de Prato

---
+++ src/main/java/br/com/allfood/restaurantesapi/Repositories/PratoRepository.java
@@ -0,0 +1,15 @@
+package br.com.allfood.restaurantesapi.Repositories;
+
+import br.com.allfood.restaurantesapi.models.entities.Prato;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PratoRepository extends JpaRepository<Prato, Long> {
+
+    @Query("SELECT obj FROM Prato obj ORDER BY obj.id")
+    Page<Prato> listarTodosOsPratos(Pageable pageable);
+}
